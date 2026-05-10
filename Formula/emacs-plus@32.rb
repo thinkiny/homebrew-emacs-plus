54a19cd987740c42be6f7e39dd058b7df832250d
thinkiny <thinkiny@126.com>
2026-05-11 01:52:50 +0800

    feat: add emacs-32

---
+++ Formula/emacs-plus@32.rb
@@ -0,0 +1,289 @@
+require_relative "../Library/EmacsBase"
+
+class EmacsPlusAT32 < EmacsBase
+  init 32
+  version "32.0.50"
+
+  desc "GNU Emacs text editor"
+  homepage "https://www.gnu.org/software/emacs/"
+
+  #
+  # Options
+  #
+
+  # Opt-out
+  option "without-cocoa", "Build a non-Cocoa version of Emacs"
+
+  # Opt-in
+  option "with-x11", "Experimental: build with x11 support"
+  option "with-debug", "Build with debug symbols and debugger friendly optimizations"
+  option "with-xwidgets", "Experimental: build with xwidgets support"
+  option "with-compress-install", "Build with compressed install optimization"
+
+  #
+  # Dependencies
+  #
+
+  depends_on "make" => :build
+  depends_on "autoconf" => :build
+  depends_on "gnu-sed" => :build
+  depends_on "gnu-tar" => :build
+  depends_on "grep" => :build
+  depends_on "awk" => :build
+  depends_on "coreutils" => :build
+  depends_on "pkg-config" => :build
+  depends_on "texinfo" => :build
+  depends_on "xz" => :build
+  depends_on "m4" => :build
+  depends_on "sqlite" => :build
+  depends_on "gnutls"
+  depends_on "librsvg"
+  depends_on "little-cms2"
+  depends_on "tree-sitter"
+  depends_on "webp"
+  depends_on "imagemagick" => :optional
+  depends_on "dbus" => :optional
+  depends_on "mailutils" => :optional
+  # `libgccjit` and `gcc` are required when Emacs compiles `*.elc` files asynchronously (JIT)
+  depends_on "libgccjit"
+  depends_on "gcc"
+
+  depends_on "gmp" => :build
+  depends_on "libjpeg" => :build
+  depends_on "zlib" => :build
+
+  if build.with? "x11"
+    depends_on "libxaw"
+    depends_on "freetype" => :recommended
+    depends_on "fontconfig" => :recommended
+  end
+
+  #
+  # Incompatible options
+  #
+
+  if build.with? "xwidgets"
+    unless (build.with? "cocoa") && (build.without? "x11")
+      odie "--with-xwidgets is not available when building --with-x11"
+    end
+  end
+
+  #
+  # URL
+  #
+
+  if ENV['HOMEBREW_EMACS_PLUS_32_REVISION']
+    url "https://github.com/emacs-mirror/emacs.git", :revision => ENV['HOMEBREW_EMACS_PLUS_32_REVISION']
+  else
+    url "https://github.com/emacs-mirror/emacs.git", :branch => "master"
+  end
+
+  #
+  # Icons
+  #
+
+  inject_icon_options
+
+  #
+  # Patches
+  #
+
+  local_patch "system-appearance", sha: "53283503db5ed2887e9d733baaaf80f2c810e668e782e988bda5855a0b1ebeb4"
+  local_patch "round-undecorated-frame", sha: "26947b6724fc29fadd44889808c5cf0b4ce6278cf04f46086a21df50c8c4151d"
+  local_patch "alpha-background", sha: "f47e5bb264a0597f53517e1b83b10a67d49e48c5e23c63af5f5c46768962e87c"
+  local_patch "glass", sha: "1116513b2b5cc644a4e711690b0443ea5100d29d965a2654e66fa469fd0af66e"
+  local_patch "xwidget-transparency", sha: "5062c465e75d5384af7b59a952f2e7df59ba19d60d88e5254dbf40f49eba08f9"
+  local_patch "xwidget-proxy", sha: "e892a0de0743b0ca16590a5ebef83147c495756697f846e038431f3a1d5eecdc"
+  local_patch "mac-font-use-typo-metrics", sha: "318395d3869d3479da4593360bcb11a5df08b494b995287074d0d744ec562c17"
+  local_patch "aggressive-read-buffering", sha: "8f5350ebc42c61ee05b7c21cad0e55e9bd2c4da49e023b74cc76fdba5595f6a3"
+
+  #
+  # Install
+  #
+
+  def install
+    args = %W[
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --enable-locallisppath=#{HOMEBREW_PREFIX}/share/emacs/site-lisp
+      --infodir=#{info}/emacs
+      --prefix=#{prefix}
+      --with-native-compilation=aot
+    ]
+
+    args << "--with-xml2"
+    args << "--with-gnutls"
+
+    args << "--without-compress-install" if build.without? "compress-install"
+
+    ENV.append "CFLAGS", "-g -Og" if build.with? "debug"
+    ENV.append "CFLAGS", "-O2 -DFD_SETSIZE=10000 -DDARWIN_UNLIMITED_SELECT"
+
+    ENV.append "CFLAGS", "-I#{Formula["sqlite"].include}"
+    ENV.append "LDFLAGS", "-L#{Formula["sqlite"].opt_lib}"
+
+    # Necessary for libgccjit library discovery
+    gcc_ver = Formula["gcc"].any_installed_version
+    gcc_ver_major = gcc_ver.major
+    gcc_lib="#{HOMEBREW_PREFIX}/lib/gcc/#{gcc_ver_major}"
+
+    ENV.append "CFLAGS", "-I#{Formula["gcc"].include}"
+    ENV.append "CFLAGS", "-I#{Formula["libgccjit"].include}"
+
+    ENV.append "LDFLAGS", "-L#{gcc_lib}"
+    ENV.append "LDFLAGS", "-Wl,-rpath,#{gcc_lib}"
+
+    args <<
+      if build.with? "dbus"
+        "--with-dbus"
+      else
+        "--without-dbus"
+      end
+
+    # Note that if ./configure is passed --with-imagemagick but can't find the
+    # library it does not fail but imagemagick support will not be available.
+    # See: https://debbugs.gnu.org/cgi/bugreport.cgi?bug=24455
+    args <<
+      if build.with?("imagemagick")
+        "--with-imagemagick"
+      else
+        "--without-imagemagick"
+      end
+
+    if build.with? "imagemagick"
+      imagemagick_lib_path = Formula["imagemagick"].opt_lib/"pkgconfig"
+      ohai "ImageMagick PKG_CONFIG_PATH: ", imagemagick_lib_path
+      ENV.prepend_path "PKG_CONFIG_PATH", imagemagick_lib_path
+    end
+
+    args << "--with-modules"
+    args << "--with-rsvg"
+    args << "--with-webp"
+    args << "--without-pop" if build.with? "mailutils"
+    args << "--with-xwidgets" if build.with? "xwidgets"
+    ENV.append "LDFLAGS", "-Wl,-framework -Wl,Network" if build.with? "xwidgets"
+
+    system "./autogen.sh"
+
+    if (build.with? "cocoa") && (build.without? "x11")
+      args << "--with-ns" << "--disable-ns-self-contained"
+
+      system "./configure", *args
+
+      # Disable aligned_alloc on Mojave. See issue: https://github.com/daviderestivo/homebrew-emacs-head/issues/15
+      if OS.mac? && MacOS.version <= :mojave
+        ohai "Force disabling of aligned_alloc on macOS <= Mojave"
+        configure_h_filtered = File.read("src/config.h")
+                                   .gsub("#define HAVE_ALIGNED_ALLOC 1", "#undef HAVE_ALIGNED_ALLOC")
+                                   .gsub("#define HAVE_DECL_ALIGNED_ALLOC 1", "#undef HAVE_DECL_ALIGNED_ALLOC")
+                                   .gsub("#define HAVE_ALLOCA 1", "#undef HAVE_ALLOCA")
+                                   .gsub("#define HAVE_ALLOCA_H 1", "#undef HAVE_ALLOCA_H")
+        File.open("src/config.h", "w") do |f|
+          f.write(configure_h_filtered)
+        end
+      end
+
+      system "gmake"
+
+      system "gmake", "install"
+
+      icons_dir = buildpath/"nextstep/Emacs.app/Contents/Resources"
+      ICONS_CONFIG.each_key do |icon|
+        next if build.without? "#{icon}-icon"
+
+        rm "#{icons_dir}/Emacs.icns"
+        resource("#{icon}-icon").stage do
+          icons_dir.install Dir["*.icns*"].first => "Emacs.icns"
+        end
+      end
+
+      # Create Emacs Client.app (AppleScript-based to handle file opening from Finder)
+      create_emacs_client_app(icons_dir)
+
+      # (prefix/"share/emacs/#{version}").install "lisp"
+      prefix.install "nextstep/Emacs.app"
+      (prefix/"Emacs.app/Contents").install "native-lisp"
+      prefix.install "nextstep/Emacs Client.app"
+
+      # inject PATH to Info.plist
+      inject_path
+
+      # inject description for protected resources usage
+      inject_protected_resources_usage_desc
+
+      # Replace the symlink with one that avoids starting Cocoa.
+      (bin/"emacs").unlink # Kill the existing symlink
+      (bin/"emacs").write <<~EOS
+        #!/bin/bash
+        exec #{prefix}/Emacs.app/Contents/MacOS/Emacs "$@"
+      EOS
+    else
+      if build.with? "x11"
+        # These libs are not specified in xft's .pc. See:
+        # https://trac.macports.org/browser/trunk/dports/editors/emacs/Portfile#L74
+        # https://github.com/Homebrew/homebrew/issues/8156
+        ENV.append "LDFLAGS", "-lfreetype -lfontconfig"
+        args << "--with-x"
+        args << "--with-gif=no" << "--with-tiff=no" << "--with-jpeg=no"
+      else
+        args << "--without-x"
+      end
+      args << "--without-ns"
+
+      system "./configure", *args
+
+      # Disable aligned_alloc on Mojave. See issue: https://github.com/daviderestivo/homebrew-emacs-head/issues/15
+      if OS.mac? && MacOS.version <= :mojave
+        ohai "Force disabling of aligned_alloc on macOS <= Mojave"
+        configure_h_filtered = File.read("src/config.h")
+                                   .gsub("#define HAVE_ALIGNED_ALLOC 1", "#undef HAVE_ALIGNED_ALLOC")
+                                   .gsub("#define HAVE_DECL_ALIGNED_ALLOC 1", "#undef HAVE_DECL_ALIGNED_ALLOC")
+                                   .gsub("#define HAVE_ALLOCA 1", "#undef HAVE_ALLOCA")
+                                   .gsub("#define HAVE_ALLOCA_H 1", "#undef HAVE_ALLOCA_H")
+        File.open("src/config.h", "w") do |f|
+          f.write(configure_h_filtered)
+        end
+      end
+
+      system "gmake"
+      system "gmake", "install"
+    end
+  end
+
+  def post_install
+    emacs_info_dir = info/"emacs"
+    Dir.glob(emacs_info_dir/"*.info") do |info_filename|
+      system "install-info", "--info-dir=#{emacs_info_dir}", info_filename
+    end
+  end
+
+  def caveats
+    <<~EOS
+      Emacs.app and Emacs Client.app were installed to:
+        #{prefix}
+
+      To link the application to default Homebrew App location:
+        osascript -e 'tell application "Finder" to make alias file to posix file "#{prefix}/Emacs.app" at posix file "/Applications" with properties {name:"Emacs.app"}'
+        osascript -e 'tell application "Finder" to make alias file to posix file "#{prefix}/Emacs Client.app" at posix file "/Applications" with properties {name:"Emacs Client.app"}'
+
+      Your PATH value was injected into Emacs.app via a wrapper script.
+      This solves the issue with macOS Sequoia ignoring LSEnvironment in Info.plist.
+
+      To disable PATH injection, set EMACS_PLUS_NO_PATH_INJECTION before running Emacs:
+        export EMACS_PLUS_NO_PATH_INJECTION=1
+
+      Report any issues to https://github.com/d12frosted/homebrew-emacs-plus
+    EOS
+  end
+
+  service do
+    run [opt_bin/"emacs", "--fg-daemon"]
+    keep_alive true
+    log_path "/tmp/homebrew.mxcl.emacs-plus.stdout.log"
+    error_log_path "/tmp/homebrew.mxcl.emacs-plus.stderr.log"
+  end
+
+  test do
+    assert_equal "4", shell_output("#{bin}/emacs --batch --eval=\"(print (+ 2 2))\"").strip
+  end
+end
